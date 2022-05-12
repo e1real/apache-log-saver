5657f60c6acb166818d79d977b295d9984649140
Esbol Moldrahmetov <esbolarai@gmail.com>
2022-05-12 12:53:10 +0600

    connect to mysql db

--- www/protected/config/database.php
+++ www/protected/config/database.php
@@ -1,14 +1,9 @@
 <?php
 
-// This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
-	'charset' => 'utf8',
-	*/
+    'connectionString' => 'mysql:host=db:8083;dbname=yii_tests',
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => 'yii_tests',
+    'charset' => 'utf8',
 );
